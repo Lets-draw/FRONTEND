b97b453624c31aec2dbfc21b63b8f21e9bac37c2
JuanC-358 <juanc358a@gmail.com>
2021-11-10 00:35:15 -0500

    palabra

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
 /*var app = (function () {
-
     class Point{
         constructor(x,y){
             this.x=x;
@@ -27,13 +26,11 @@
     var setId = function(event){
         id = event.target.value;
     }
-
     var eventClick = function (event){
         const pt = getMousePosition(event);
         addPointToCanvas(pt);
         if(id) stompClient.send(`/topic/newpoint.${id}`, {}, JSON.stringify(pt));
     }
-
     var addPointToCanvas = function (point) {        
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
@@ -51,8 +48,6 @@
             y: evt.clientY - rect.top
         };
     };
-
-
     var connectAndSubscribe = function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
@@ -67,29 +62,23 @@
                 addPointToCanvas(theObject);
             });
         });
-
     };
     
     
-
     return {
-
         init: function () {
             var can = document.getElementById("canvas");
             pointMouse();
             //websocket connection
             connectAndSubscribe();
         },
-
         publishPoint: function(px,py){
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
-
             //publicar el evento
              stompClient.send(`/topic/newpoint.${id}`, {}, JSON.stringify(pt));
         },
-
         disconnect: function () {
             if (stompClient !== null) {
                 stompClient.disconnect();
@@ -103,7 +92,6 @@
             connectAndSubscribe();
         }
     };
-
 })();*/
 async function myFunction() {
     const queryString = window.location.search;
@@ -120,11 +108,9 @@ async function myFunction() {
   );
   var str = response;
   console.log(response);
-    //for(var i = 0;i<str.user.length;i++)
-    for(var i = 0;i<2;i++)
+   for(var i = 0;i<str.user.length;i++)
    {
        console.log(str.user[i].nikname);
-       console.log(document.getElementById(i+1));
        var nombre = str.user[i].nikname;
        var skin = str.user[i].skin;
        var points = str.user[i].points;
@@ -133,4 +119,12 @@ async function myFunction() {
       //document.getElementById(i+1).textContent= string;
 
    }
+   var getPalabra=  await  fetch(`http://localhost:8081/getWord/`+page_type+'/').then((rest)=>{
+      if (!rest.ok) throw new Error("Response is NOT ok");
+      return rest.json();
+  }
+  );
+  console.log(getPalabra);
+  console.log(getPalabra.name)
+   document.getElementById("word").innerText = "Palabra : "+ getPalabra.name;
 }
