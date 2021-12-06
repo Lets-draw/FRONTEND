865fdae6a4db960298e7804c71cbaac16fbbb8a1
JuanC-358 <juanc358a@gmail.com>
2021-12-06 16:18:40 -0500

    FrontFix

--- public/app.js
+++ public/app.js
@@ -141,6 +141,7 @@ async function myFunction() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     var rect = canvas.getBoundingClientRect();
+    
     var x = 0, y = 0, dibujando = false, color = 'black', grosor = 1;
     function defcolor(c) {
         color = c;
