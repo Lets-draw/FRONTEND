100009493c78531f8ba3553311f1ca0dda8a609c
JuanC-358 <juanc358a@gmail.com>
2021-12-08 00:01:25 -0500

    canvas3

--- public/app.js
+++ public/app.js
@@ -343,7 +343,7 @@ async function start() {
         return res;
     }
     );
-    //clearInterval(timer);
+    clearInterval(timer);
     var canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
