09c7cd1665c95fb7ee9fb877259f286469648f17
JuanC-358 <juanc358a@gmail.com>
2021-12-08 11:03:23 -0500

    canvas7

--- public/app.js
+++ public/app.js
@@ -93,54 +93,12 @@
         }
     };
 })();*/
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var rect = canvas.getBoundingClientRect();
-
-var x = 0, y = 0, dibujando = false, color = 'black', grosor = 1;
-function defcolor(c) {
-    color = c;
-}
-function defgrosor(g) {
-    grosor = g;
-}
-canvas.addEventListener('mousedown', function (e) {
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
-    dibujando = true;
-});
-
-canvas.addEventListener('mousemove', function (e) {
-    if (dibujando === true) {
-        dibujar(x, y, e.clientX - rect.left, e.clientY - rect.top);
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-    }
-
-});
-
-canvas.addEventListener('mouseup', function (e) {
-    if (dibujando === true) {
-        dibujar(x, y, e.clientX - rect.left, e.clientY - rect.top);
-        x = 0;
-        y = 0;
-        dibujando = false;
-    }
-});
-function dibujar(x1, y1, x2, y2) {
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = grosor;
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    ctx.closePath();
-}
 var numMessages = 0;
 var numUsers=0;
 var seconds = 60;
 var time = true;
 var timer = true;
+var x = 0, y = 0, dibujando = false, color = 'black', grosor = 1;
 async function myFunction() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -182,10 +140,54 @@ async function myFunction() {
     //console.log(getPalabra.name);
     document.getElementById("word").innerText = "Palabra : " + getPalabra.name;*/
     //window.setInterval (myFunction(), 1000);
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var rect = canvas.getBoundingClientRect();
+    
+    
+    
+    canvas.addEventListener('mousedown', function (e) {
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+        dibujando = true;
+    });
+
+    canvas.addEventListener('mousemove', function (e) {
+        if (dibujando === true) {
+            dibujar(x, y, e.clientX - rect.left, e.clientY - rect.top);
+            x = e.clientX - rect.left;
+            y = e.clientY - rect.top;
+        }
+
+    });
+
+    canvas.addEventListener('mouseup', function (e) {
+        if (dibujando === true) {
+            dibujar(x, y, e.clientX - rect.left, e.clientY - rect.top);
+            x = 0;
+            y = 0;
+            dibujando = false;
+        }
+    });
     
     setInterval('getMessages()',1000);
     setInterval('getUsers()',1000);
     time = setInterval(showRemaining, 1000);
+}
+function dibujar(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = grosor;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+function defcolor(c) {
+    color = c;
+}
+function defgrosor(g) {
+    grosor = g;
 }
 async function sendMessage() {
     const queryString = window.location.search;
